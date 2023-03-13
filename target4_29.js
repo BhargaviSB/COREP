1ca3264181a8980ad0257f6b55eb24a4be5d9617
BhargaviSB <125358912+BhargaviSB@users.noreply.github.com>
2023-03-13 13:24:17 +0530

    Update and rename target3_29.js to target4_29.js

--- target4_29.js
+++ target4_29.js
@@ -91,7 +91,7 @@ looker.plugins.visualizations.add({
     downloadButton.setAttribute('title', 'Download As Excel'); 
     this._container.prepend(downloadButton);
     downloadButton.addEventListener('click', (event) => {
-      var uri = 'data:application/vnd.ms-excel;base64,'
+      var uri = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,'
         , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{Worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
         , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
         , format = function (s, c) {
