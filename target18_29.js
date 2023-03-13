5dd26cd0d769bfe1928b9b466c9708341951d4d1
BhargaviSB <125358912+BhargaviSB@users.noreply.github.com>
2023-03-13 17:18:14 +0530

    Update and rename target17_29.js to target18_29.js

--- target18_29.js
+++ target18_29.js
@@ -91,8 +91,8 @@ looker.plugins.visualizations.add({
     downloadButton.setAttribute('title', 'Download As Excel'); 
     this._container.prepend(downloadButton);
     downloadButton.addEventListener('click', (event) => {
-      var uri = "data:application/vnd.ms-excel;base64,"
-        , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{Worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="application/vnd.ms-excel;base64,"/></head><body><table>{table}</table></body></html>'
+      var uri = 'data:application/vnd.ms-excel;base64,'
+        , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table x:str>{table}</table></body></html>'
         , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
         , format = function (s, c) {
           const regex = /style="([^"]*)"/g;
