ba39770d163753e51ffc96529468c0c6e7924c93
BhargaviSB <125358912+BhargaviSB@users.noreply.github.com>
2023-03-20 13:24:15 +0530

    Update and rename 26_dialogue_32.js to 26_dialogue_33.js

--- 26_dialogue_33.js
+++ 26_dialogue_33.js
@@ -109,12 +109,16 @@ looker.plugins.visualizations.add({
                 type: 'binary'
             });  
 
+            // method:4
+            var uriContent = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet," + encodeURIComponent(wbexport);
+            window.open(uriContent, "export26")
+
             // method:3
-            var blob = new Blob ([wbexport], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
-            var link = document.createElement('a');
-            link.href = window.URL.createObjectURL(blob);
-            link.download = "table26.xlsx";
-            link.click();
+            // var blob = new Blob ([wbexport], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
+            // var link = document.createElement('a');
+            // link.href = window.URL.createObjectURL(blob);
+            // link.download = "table26.xlsx";
+            // link.click();
                 
             // method:2
             // var blob = new Blob([wbexport], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
@@ -128,16 +132,16 @@ looker.plugins.visualizations.add({
             // window.saveAs(blob, fileName);
 
             // method:1
-//             saveAs(new Blob([s2ab(wbexport)], {
-//                 type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-//             }), 'export26.xlsx');
+            // saveAs(new Blob([s2ab(wbexport)], {
+            //     type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            // }), 'export26.xlsx');
 
-//             function s2ab(s) {
-//                 var buf = new ArrayBuffer(s.length);
-//                 var view = new Uint8Array(buf);
-//                 for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
-//                 return buf;
-//             }
+            // function s2ab(s) {
+            //     var buf = new ArrayBuffer(s.length);
+            //     var view = new Uint8Array(buf);
+            //     for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+            //     return buf;
+            // }
 
             // var file = XLSX.utils.table_to_book(data, {sheet: "Sheet26"});
             // XLSX.write(file, {bookType: type, bookSST: true, type: 'base64'});
