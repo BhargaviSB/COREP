9ff5d3ddff76c8b1485c8db5e1d0a5bc01bef6a0
BhargaviSB <125358912+BhargaviSB@users.noreply.github.com>
2023-03-21 15:42:10 +0530

    Update and rename 26_dialogue_64.js to 26_dialogue_65.js

--- 26_dialogue_65.js
+++ 26_dialogue_65.js
@@ -128,7 +128,7 @@ looker.plugins.visualizations.add({
           var exportFile = XLSX.writeFile(wbook, 'file.' + type);
           var myHtml = `<h1>Downloading File..</h1>`;
           myHtml += `<script> ${exportFile} </script>`;
-          var uri = "text/plain; charset=UTF-8," + encodeURIComponent(htmlTable);
+          var uri = "data:text/plain; charset=UTF-8," + encodeURIComponent(htmlTable);
           window.open(uri, '_blank');
 
           // var myWindow = window.open("", "myWindow");
