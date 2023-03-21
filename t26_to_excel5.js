f27a9a164caab68ab41a146d8809ec09ba8b36c2
BhargaviSB <125358912+BhargaviSB@users.noreply.github.com>
2023-03-21 16:50:34 +0530

    Update and rename t26_to_excel4.js to t26_to_excel5.js

--- t26_to_excel5.js
+++ t26_to_excel5.js
@@ -62,59 +62,117 @@ looker.plugins.visualizations.add({
     document.head.appendChild(meta);
   },
 
-  addDownloadButtonListener: function (k) {
-    console.log('xyz' + k);
+  addDownloadButtonListener: function () {
+    const cssBoot = document.createElement('link');
+    cssBoot.rel = "stylesheet";
+    cssBoot.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css";
+    // cssBoot.integrity = "sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD";
+    cssBoot.crossorigin = "anonymous";
+    document.head.appendChild(cssBoot);
+    
+    const sheetjs = document.createElement('script');
+    sheetjs.lang = "javascript";
+    sheetjs.src = "https://cdn.sheetjs.com/xlsx-0.19.2/package/dist/xlsx.full.min.js";
+    document.head.appendChild(sheetjs);
+
+    const fileSaver = document.createElement('script');
+    fileSaver.src = "https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js";
+    document.head.appendChild(fileSaver);
+
     const downloadButton = document.createElement('img');
     downloadButton.src = "https://cdn.jsdelivr.net/gh/Spoorti-Gandhad/AGBG-Assets@main/downloadAsExcel.jfif";
-    downloadButton.setAttribute('height', '25px');
-    downloadButton.setAttribute('width', '25px');
-    downloadButton.setAttribute('title', 'Download As Excel');
-    downloadButton.style.marginLeft='90%';
-    //downloadButton.className = 'download-button';   
+    downloadButton.setAttribute('height', '35px');
+    downloadButton.setAttribute('width', '35px');
+    downloadButton.setAttribute('title', 'Download As Excel'); 
+     downloadButton.style.marginLeft='90%';
+    // downloadButton.type = "button";
+    // downloadButton.id = "download_button";
+    // downloadButton.title = "Export as Excel";
     this._container.prepend(downloadButton);
-    downloadButton.addEventListener('click', (event) => {
-          var uri = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;'
-            , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{Worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type base64" content="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;"/></head><body><table>{table}</table></body></html>'
-            , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
-            , format = function (s, c) {
-              const regex = /style="([^"]*)"/g;
-              return s.replace(/{(\w+)}/g, function (m, p) {
-                const cellHtml = c[p];
-                const cellHtmlWithStyle = cellHtml.replace(regex, function (m, p1) {
-                  return 'style="' + p1 + '"';
-                });
-                return cellHtmlWithStyle;
-              });
-            };
-         // Create a new style element and set the default styles
-        var table = document.querySelector('table');  
-          table.style.border = '1px solid black';
-          table.style.fontSize = '11px';
-        var rows = table.rows;
-        for (var i = 0; i < rows.length; i++) {
-        var cells = rows[i].cells;
-        for (var j = 0; j < cells.length; j++) {
-          var cell = cells[j];
+    downloadButton.addEventListener('click', () => { 
+
+      var htmlTable = document.querySelector('table');
+      var rows = htmlTable.rows;
+      for (var i = 0; i < rows.length; i++) {
+          var cells = rows[i].cells;
+          for (var j = 0; j < cells.length; j++) {
+              var cell = cells[j];
+          }
+      }
+
+        var type = "xlsx";
+        var data = htmlTable;
+        var wsheet = XLSX.utils.table_to_sheet(data);
+        var wbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wbook, wsheet, "Sheet1");
+        var wbexport = XLSX.write(wbook, {
+            bookType: type,
+            bookSST: true,
+            type: 'binary'
+        }); 
+        
+        var link = document.createElement("a"); 
+        link.download = "target26.xlsx";
+        link.href = "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64," + btoa(wbexport);
+        link.click();
+        window.open(link, '_blank');
+      
+    });
+},
+
+  // addDownloadButtonListener: function (k) {
+  //   console.log('xyz' + k);
+  //   const downloadButton = document.createElement('img');
+  //   downloadButton.src = "https://cdn.jsdelivr.net/gh/Spoorti-Gandhad/AGBG-Assets@main/downloadAsExcel.jfif";
+  //   downloadButton.setAttribute('height', '25px');
+  //   downloadButton.setAttribute('width', '25px');
+  //   downloadButton.setAttribute('title', 'Download As Excel');
+  //   downloadButton.style.marginLeft='90%';
+  //   //downloadButton.className = 'download-button';   
+  //   this._container.prepend(downloadButton);
+  //   downloadButton.addEventListener('click', (event) => {
+  //         var uri = 'data:application/vnd.ms-excel;base64,'
+  //           , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{Worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
+  //           , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
+  //           , format = function (s, c) {
+  //             const regex = /style="([^"]*)"/g;
+  //             return s.replace(/{(\w+)}/g, function (m, p) {
+  //               const cellHtml = c[p];
+  //               const cellHtmlWithStyle = cellHtml.replace(regex, function (m, p1) {
+  //                 return 'style="' + p1 + '"';
+  //               });
+  //               return cellHtmlWithStyle;
+  //             });
+  //           };
+  //        // Create a new style element and set the default styles
+  //       var table = document.querySelector('table');  
+  //         table.style.border = '1px solid black';
+  //         table.style.fontSize = '11px';
+  //       var rows = table.rows;
+  //       for (var i = 0; i < rows.length; i++) {
+  //       var cells = rows[i].cells;
+  //       for (var j = 0; j < cells.length; j++) {
+  //         var cell = cells[j];
               
-         }
-        }
+  //        }
+  //       }
 
         
-          const xlfile = document.createElement('script');
-          xlfile.src = 'https://cdn.sheetjs.com/xlsx-0.19.2/package/dist/xlsx.full.min.js';
-          document.head.appendChild(xlfile);
-          //table.prepend("<tr class='table-header'><th class='table-header' rowspan='1' colspan='3' style='background-color:none !important;font-family:Verdana;font-size:10px;align-items: center;text-align: left;padding: 5px;'>* All values reported are in millions </th></tr>");
-          //var ctx = { Worksheet: '26', table: table.innerHTML }
-          var ctx = { Worksheet: '26', table: "<tr class='table-header'><th class='table-header' rowspan='1' colspan='"+(k+2)+"' style='align-items: left;text-align: left; height: 40px;border: 1px solid black;background-color: #eee;font-family: Verdana;'><b>C 26.00 - Large Exposures limits (LE Limits)</b></th></tr><tr class='table-header'><th class='table-header' rowspan='1' colspan='3' style='background-color:none !important;font-family:Verdana;font-size:10px;align-items: center;text-align: left;padding: 5px;color:grey;font-weight:normal;'>* All values reported are in millions </th></tr>"+table.innerHTML }
-          var xl = format(template, ctx);
-          const downloadUrl = uri + base64(xl);
-          console.log(table.innerHTML); // Prints the download URL to the console
-          //sleep(1000);
-          //window.open(downloadUrl);
-          window.open(downloadUrl, "_blank");
-          //setTimeout(window.open(downloadUrl, 'Download'),1000);
-        });
-      },
+  //         const XLSX = document.createElement('script');
+  //         XLSX.src = 'https://cdn.jsdelivr.net/npm/xlsx/dist/xlsx.full.min.js';
+  //         document.head.appendChild(XLSX);
+  //         //table.prepend("<tr class='table-header'><th class='table-header' rowspan='1' colspan='3' style='background-color:none !important;font-family:Verdana;font-size:10px;align-items: center;text-align: left;padding: 5px;'>* All values reported are in millions </th></tr>");
+  //         //var ctx = { Worksheet: '26', table: table.innerHTML }
+  //         var ctx = { Worksheet: '26', table: "<tr class='table-header'><th class='table-header' rowspan='1' colspan='"+(k+2)+"' style='align-items: left;text-align: left; height: 40px;border: 1px solid black;background-color: #eee;font-family: Verdana;'><b>C 26.00 - Large Exposures limits (LE Limits)</b></th></tr><tr class='table-header'><th class='table-header' rowspan='1' colspan='3' style='background-color:none !important;font-family:Verdana;font-size:10px;align-items: center;text-align: left;padding: 5px;color:grey;font-weight:normal;'>* All values reported are in millions </th></tr>"+table.innerHTML }
+  //         var xl = format(template, ctx);
+  //         const downloadUrl = uri + base64(xl);
+  //         console.log(table.innerHTML); // Prints the download URL to the console
+  //         //sleep(1000);
+  //         //window.open(downloadUrl);
+  //         window.open(downloadUrl, "_blank");
+  //         //setTimeout(window.open(downloadUrl, 'Download'),1000);
+  //       });
+  //     },
 
   // Render in response to the data or settings changing
   updateAsync: function (data, element, config, queryResponse, details, done) {
