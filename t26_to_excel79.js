9b42277f837a9928cf4869f56047254e10e3570a
BhargaviSB <125358912+BhargaviSB@users.noreply.github.com>
2023-03-31 15:59:15 +0530

    Update and rename t26_to_excel78.js to t26_to_excel79.js

--- t26_to_excel79.js
+++ t26_to_excel79.js
@@ -174,7 +174,7 @@ looker.plugins.visualizations.add({
         // document.write("<span style='font-family:serif; text-align: left; font-weight:normal; font-size:10px; align-items:left; border:1px solid black; background-color: #eee;'>"+note+"</span>");
         // note[0].style.font = "10pt serif";
         var header = [
-          {v: "C 26.00 - Large Exposures limits (LE Limits)", t: "s", s: {font: {name: "Calibri", sz: 16, bold: true}, fill: {bgColor: {rgb: "#D9D9D9"}}}}
+          {v: "C 26.00 - Large Exposures limits (LE Limits)", t: "s", s: {font: {name: "Calibri", sz: 16, bold: true}, fill: {bgColor: {rgb: "#C8C8C8"}}}}
         ];
         var note = [
           {v: "* All values reported are in millions", t: "s", s: {font: {name: "Calibri", sz: 10}}}
@@ -189,7 +189,10 @@ looker.plugins.visualizations.add({
         XLSX.utils.sheet_add_aoa(wsheet, [note], { origin: 'A2' });
         var wbook = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(wbook, wsheet, "C26");
-        wsheet["!merges"] = [{s:{c:0, r:0}, e:{c:9, r:0}}];
+        wsheet["!merges"] = [{s:{c:0, r:0}, e:{c:9, r:0}}, 
+          {s:{c:0, r:0}, e:{c:10, r:0}}, 
+          {s:{c:1, r:1}, e:{c:10, r:1}}, 
+          {s:{c:0, r:3}, e:{c:1, r:4}}];
         // wsheet["!cols"] = [{wch: 15}];
         var wbexport = XLSX.write(wbook, {
             bookType: type,
