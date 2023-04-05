6023fa7699ceb21b4a709556b2fcd3ecbd6a1019
BhargaviSB <125358912+BhargaviSB@users.noreply.github.com>
2023-04-05 16:37:03 +0530

    Update and rename target26_28.js to target27_28.js

--- target27_28.js
+++ target27_28.js
@@ -135,12 +135,10 @@ looker.plugins.visualizations.add({
       for (var i = 0; i < l; i++) {
           var cells = rows[i].cells;
           for (var j = 0; j < cells.length; j++) {
-            //   var cell = cells[j];
-              cells[j].innerHTML = {t: "s", s: {border: {top: {style: "medium"}, left: {style: "medium"}, bottom: {style: "medium"}, right: {style: "medium"}}}};
+              var cell = cells[j];
+              // cells[j].innerHTML = {t: "s", s: {border: {top: {style: "medium"}, left: {style: "medium"}, bottom: {style: "medium"}, right: {style: "medium"}}}};
           }
       }
-
-      htmlTable.querySelector('th').style.backgroundColor = "blue";
 
       // var theader = "<tr class='table-header'><th class='table-header' rowspan='1' colspan='100' style='align-items: left;text-align: left; height: 40px;border: 1px solid black;background-color: #eee;font-family: Verdana;'><b>C 29.00 - Detail of the exposures to individual clients within groups of connected clients (LE 3)</b></th></tr><tr class='table-header'><th class='table-header' rowspan='1' colspan='3' style='background-color:none !important;font-family:Verdana;font-size:10px;align-items: center;text-align: right;padding: 5px;color:grey;font-weight:normal;'>* All values reported are in millions </th></tr>";
       // var theader = document.createElement('h1');
